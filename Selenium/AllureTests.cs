eb510272977cf255c63d455a79d45f2bf6d6246b
alfabettagamak <alisa.skrynko@gmail.com>
2022-10-21 20:51:04 +0300

    repots

---
+++ Selenium/AllureTests.cs
@@ -0,0 +1,52 @@
+﻿using Allure.Commons;
+using Newtonsoft.Json.Linq;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+
+namespace Selenium
+{
+
+    //https://docs.nunit.org/articles/nunit/writing-tests/attributes/parallelizable.html
+    [TestFixture]
+    [AllureNUnit]
+    //  [Parallelizable(scope: ParallelScope.All )]
+    public class AllureTests : TestBase
+    {
+
+        private bool isNeedAuth = true;
+        private JObject testUser;
+        private bool isNeedScreenEtalon = false;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            //driver = new ChromeDriver();
+            testUser = (JObject) testData["testUser2"];
+            driver.Navigate().GoToUrl("https://old.kzn.opencity.pro/");
+            Authorization(testUser["login"].ToString(), testUser["password"].ToString());
+        }
+        
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+
+        [Test(Description = "XXX")]
+        [AllureTag("Regression")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureIssue("ISSUE-1")]
+        [AllureTms("TMS-12")]
+        [AllureOwner("User")]
+        [AllureSuite("PassedSuite")]
+        [AllureSubSuite("NoAssert")]
+        public void OpenKznAuth1Testing()
+        {
+            var element = driver.FindElement(By.XPath("//div[@class='username']/span"));
+            Assert.AreEqual(testUser["login"].ToString(), element.Text);
+        }
+    }
+}
---
+++ Selenium/OneTimeSetup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class OneTimeSetup
+{
+    [OneTimeSetUp]
+    public void Init()
+    {
+        string directory = AppDomain.CurrentDomain.BaseDirectory;
+        Environment.CurrentDirectory = directory.Substring(0, directory.IndexOf("bin"));
+    }
+}
